8a3ef86e6ce60d01e687ba11c7c9239a02fa8527
Rhab <rmalsulami0001@stu.kau.edu.sa>
2024-05-06 21:10:05 +0300

    viewGolfWorkingHourSchedule() method

--- src/Schedule.java
+++ src/Schedule.java
@@ -37,5 +37,42 @@ public class Schedule {
         this.end_time = end_time;
     }
     
+    public static void viewGolfWorkingHourSchedule() {
+       System.out.println("Working Hours Schedule for Golf Cars:");
+        System.out.println("Time        | Activity");
+        System.out.println("----------------------------------------------");
+        
+        // Start of shift
+        printSchedule("7:30 am", "Start of shift");
+        printAvailableCars("7:30 am", 2);
+        
+        // First service
+        printSchedule("8:00 am", "First service");
+        printAvailableCars("10:00 am", 3);
+        
+        // Break
+        printSchedule("11:00 am", "Break");
+        
+        // Second service
+        printSchedule("11:30 am", "Second service");
+        printAvailableCars("12:30 pm", 5);
+        
+        // Fourth service
+        printSchedule("12:30 pm", "Fourth service");
+        printAvailableCars("1:30 pm", 3);
+        
+        // End of shift
+        printSchedule("2:30 pm", "End of shift");
+    }
+
+    private static void printSchedule(String time, String activity) {
+        System.out.printf("%-12s | %-30s%n", time, activity);
+    }
+
+    private static void printAvailableCars(String time, int availableCars) {
+        System.out.printf("%-12s | Number of available cars: %d%n", time, availableCars);
+    }  
+    }
     
-}
+    
+
